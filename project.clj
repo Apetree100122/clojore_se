9fcb693078946c003722b92bd8f3cc3959b633d8
Alexander Petree <apetree1001@email.phoenix.edu>
2024-01-04 15:50:30 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject buddy "2.0.0"
-  :description "Security library for Clojure"
-  :url "https://github.com/funcool/buddy"
-  :license {:name "Apache 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[buddy/buddy-core "1.4.0" :exclusions [org.clojure/clojure]]
-                 [buddy/buddy-auth "2.1.0" :exclusions [org.clojure/clojure]]
-                 [buddy/buddy-hashers "1.3.0" :exclusions [org.clojure/clojure]]
-                 [buddy/buddy-sign "2.2.0" :exclusions [org.clojure/clojure]]]
-  :plugins [[lein-ancient "0.6.10"]]
-  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"])
+(defproject buddy "2":description "Security library for Clojure"
+:url "https://github.com/funcool/buddy":license :name "Apache 2.0"
+:url "http://www.apache.org/licenses/LICENSE:dependencies [[buddy/buddy-core "3" 
+:exclusions [org.clojure/clojure]]
+[buddy/buddy-auth "3" 
+:exclusions [org.clojure/clojure]]
+[buddy/buddy-hashers 
+"2" :exclusions [org.clojure/clojure]]
+[buddy/buddy-sign 
+ "4" :exclusions [org.clojure/clojure]]] :plugins [[lein-ancient "0.4"]] :javac-options ["-target" "6" "-source""6""-Xlint:-options"])
